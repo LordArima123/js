d7f2b1c4aab1100bbee4246fba378c3db217a4fc
LordArima123 <phongpham1312@gmail.com>
2024-04-14 14:07:31 +0200

    finish

--- express-server-3/main.js
+++ express-server-3/main.js
@@ -1,112 +1,144 @@
-import express from 'express';
+import express from "express";
 import knex from "knex";
-import knexfile from './knexfile.js';
+import knexfile from "./knexfile.js";
 
 const app = express();
 const db = knex(knexfile);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-	console.log('Incomming request', req.method, req.url);
-	next();
+  console.log("Incomming request", req.method, req.url);
+  next();
 });
 
-app.get('/', async (req, res) => {
-	const todos = await db('todos').select("*");
-	console.log(todos);
-	res.render('index', {
-		title: 'Todo List',
-		todos: todos,
-	});
+app.get("/", async (req, res) => {
+  const todos = await db("todos").select("*").orderBy("piority", "id");
+  const piority = ["Now", "High", "Moderate", "Low", "Latter"];
+  const newtodos = todos.map((todo) => {
+    // const newtodo = {
+    //   id: todo.id,
+    //   title: todo.title,
+    //   done: todo.done,
+    //   piority: piority[todo.piority - 1],
+    // };
+
+    const newtodo = { ...todo, piority: piority[todo.piority - 1] };
+    return newtodo;
+  });
+
+  res.render("index", {
+    title: "Todo List",
+    todos: newtodos,
+  });
 });
 
-app.get('/todo/:id', async (req, res) => {
-	
-	const todos = await db('todos').select("*");
-	const todo = todos.find((todo) => {
-		return todo.id === Number(req.params.id)});
-	if (!todo) {
-		return res.render('404error');
-	};
-	res.render('todo', {
-		title: 'Todo ',
-		todo: todo,
-	});
+app.get("/todo/:id", async (req, res) => {
+  const todos = await db("todos").select("*");
+  const piority = ["Now", "High", "Moderate", "Low", "Latter"];
+  console.log(todos);
+  const todo = todos.find((todo) => {
+    return todo.id === Number(req.params.id);
+  });
+  if (!todo) {
+    return res.render("404error");
+  }
+  const newtodo = { ...todo, piority: piority[todo.piority - 1] };
+
+  res.render("todo", {
+    title: "Todo ",
+    todo: newtodo,
+  });
 }); //go to todo id
 
-app.post('/update-todo', async (req, res) => {
-	const todos = await db('todos').select("*");
+app.post("/update-todo", async (req, res) => {
+  const todos = await db("todos").select("*");
 
-	const todo = todos.find((todo) => {
-		return todo.id === Number(req.body.id);
-	});
+  const todo = todos.find((todo) => {
+    return todo.id === Number(req.body.id);
+  });
 
-	if (!todo || !todo.id || !req.body.title) {
-		return res.redirect(`todo/${todo.id}`);
-	}
-	
-	todo.title = req.body.title;
-	await db('todos').where("id",todo.id).update({title:todo.title});
-	
-	res.redirect(`/todo/${todo.id}`);
+  if (!todo || !todo.id || !req.body.title) {
+    return res.redirect(`todo/${todo.id}`);
+  }
+
+  todo.title = req.body.title;
+  await db("todos").where("id", todo.id).update({ title: todo.title });
+
+  res.redirect(`/todo/${todo.id}`);
 }); //update todo
 
+app.post("/piority", async (req, res) => {
+  const todos = await db("todos").select("*");
 
-app.post('/add-todo',async (req, res) => {
-	const todo = {
-		title: req.body.title,
-		done: false,
-	}
-	if (!todo.title) {
-		return res.redirect('/');
-	};
-	await db('todos').insert(todo);
-	res.redirect('/');
-}); //change exist id and push new todo
+  const todo = todos.find((todo) => {
+    return todo.id === Number(req.body.id);
+  });
 
-app.get('/remove-todo/:id', async (req, res) => {
-	const todos = await db('todos').select("*");
-	const todo = todos.find((todo) => {
-		return todo.id === Number(req.params.id);
-	});
-	if (!todo.title) {
-		return res.redirect('/');
-	};
-	await db('todos').where("id",todo.id).del();
+  if (!todo || !todo.id) {
+    return res.redirect(`todo/${todo.id}`);
+  }
 
-	res.redirect('/');
+  todo.piority = req.body.piority;
+
+  await db("todos").where("id", todo.id).update({ piority: todo.piority });
+
+  res.redirect(`/todo/${todo.id}`);
 });
 
-app.get('/toggle-todo/:id', async (req, res) => {
-	const todos = await db('todos').select("*");
-	const todo = todos.find((todo) => {
-		return todo.id === Number(req.params.id);
-	});
-	if (!todo) {
-		return res.redirect('/');
-	};
+app.post("/add-todo", async (req, res) => {
+  const todo = {
+    title: req.body.title,
+    done: false,
+  };
+  if (!todo.title) {
+    return res.redirect("/");
+  }
+  await db("todos").insert(todo);
+  res.redirect("/");
+}); //change exist id and push new todo
 
-	todo.done = !todo.done;
-	await db("todos").where("id",todo.id).update({done:todo.done});
-	res.redirect('/');
+app.get("/remove-todo/:id", async (req, res) => {
+  const todos = await db("todos").select("*");
+  const todo = todos.find((todo) => {
+    return todo.id === Number(req.params.id);
+  });
+  if (!todo.title) {
+    return res.redirect("/");
+  }
+  await db("todos").where("id", todo.id).del();
+
+  res.redirect("/");
+});
+
+app.get("/toggle-todo/:id", async (req, res) => {
+  const todos = await db("todos").select("*");
+  const todo = todos.find((todo) => {
+    return todo.id === Number(req.params.id);
+  });
+  if (!todo) {
+    return res.redirect("/");
+  }
+
+  todo.done = !todo.done;
+  await db("todos").where("id", todo.id).update({ done: todo.done });
+  res.redirect("/");
 });
 
 app.use((req, res) => {
-	res.status(404);
-	res.render('404error');
+  res.status(404);
+  res.render("404error");
 });
 
 app.use((err, req, res, next) => {
-	console.error(err);
-	res.status(500);
-	res.render('500error');
+  console.error(err);
+  res.status(500);
+  res.render("500error");
 });
 
 app.listen(8000, () => {
-	console.log('Server listening on http://localhost:8000');
+  console.log("Server listening on http://localhost:8000");
 });
-
